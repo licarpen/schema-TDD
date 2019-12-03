54da5bee19f738105578067af7b9f894b191332c
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-02 16:37:49 -0800

    fixed mistakes that did not exist

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const { isNumber, isString, isBoolean, isArray, isObject, isFunction } = require('./lib/types.js');
+const {
+  isNumber,
+  isString, 
+  isBoolean, 
+  isArray, 
+  isObject, 
+  isFunction, 
+  getCaster, 
+  castToNumber,
+  castToString,
+  castToBoolean,
+  castToArray } = require('./lib/types.js');
 
 console.log(isNumber(3));
 console.log(isString('3'));
@@ -6,4 +17,10 @@ console.log(isBoolean(true));
 console.log(isArray([1, 2]));
 console.log(isObject({ 'me':1, 'you':2 }));
 console.log(isFunction(() => {}));
+console.log(getCaster(Number));
+console.log(castToNumber('3'));
+console.log(castToString(3));
+console.log(castToBoolean('true'));
+console.log(castToArray('Hi!'));
+
 
