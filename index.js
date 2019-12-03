9131fdbb1632cee6cd7a744748ad01efc309f5eb
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-02 16:15:24 -0800

    added functions to index

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const { isNumber } = require('./lib/types.js');
+const { isNumber, isString, isBoolean, isArray, isObject, isFunction, CastError, getCaster, castToNumber, castToString, castToBoolean, castToArray } = require('./lib/types.js');
 
-console.log(isNumber('3'));
+console.log(isNumber(3));
+console.log(isString('3'));
+console.log(isBoolean(true));
+console.log(isArray([1, 2]));
+console.log(isObject({ 'me':1, 'you':2 }));
+console.log(isFunction(() => {}));
+
